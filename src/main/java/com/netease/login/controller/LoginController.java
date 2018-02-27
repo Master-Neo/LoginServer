826bd72c6f164ae062361ce26144332d07b554b7
hzxiexinqi <hzxiexinqi@corp.netease.com>
2018-02-27 20:20:07 +0800

    若干修改

--- src/main/java/com/netease/login/controller/LoginController.java
+++ src/main/java/com/netease/login/controller/LoginController.java
@@ -1,6 +1,8 @@
 package com.netease.login.controller;
 
-import com.netease.login.entity.User;
+import com.netease.login.entity.request.User;
+import com.netease.login.entity.base.BaseResponse;
+import com.netease.login.entity.response.LoginResult;
 import com.netease.login.service.UserServiceImpl;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by neo on 2018/2/20.
@@ -31,16 +34,25 @@ public class LoginController {
     }
 
     @PostMapping(value = "/login")
-    public void realLogin(@ModelAttribute User user) {
+    public @ResponseBody
+    BaseResponse<LoginResult> realLogin(@ModelAttribute User user) {
         if (null == user) {
             LOG.error(user.getAccountId());
-            return;
         }
         LOG.info(user.getAccountId() + " : " + user.getPassword());
+        BaseResponse<LoginResult> response = new BaseResponse<>();
+        LoginResult result = new LoginResult();
         if (mUserService.login(user)) {
             LOG.info("login success.");
+            // TODO: 2018/2/27 生成sessionId，作为cookie返回给h5
+            response.setCode("200");
+            result.setSuccess(true);
         } else {
             LOG.error("login failed.");
+            // TODO: 将登陆结果返回给前端
+            response.setCode("-1");
+            result.setSuccess(false);
         }
+        return response;
     }
 }
